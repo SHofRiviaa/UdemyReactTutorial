6871671422fda03dac4f965f48677cd4795cb8c3
SHofRiviaa <salmanhaidar9576@gmail.com>
2024-04-25 21:47:15 -0400

    finished sect 6 about state, events and forms,interactive components

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -17,11 +19,46 @@ export default function App() {
 function Logo() {
   return <h1>🏜️ Far away in Dune 🏜️</h1>;
 }
+
+//TO implement the controlled element technique, we need to add a state to the Form component
+// 1 create a state variable to hold the value of the input field
+// 2 pass the value of the input field to the input element
+// 3 add an event handler to the input element to update the state variable like onChange={(e) => setItem(e.target.value)}
 function Form() {
+  const [item, setItem] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!item) return;
+
+    const newItem = { item, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setItem("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for Dune?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => (
+          <option key={i}>{i + 1}</option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -29,7 +66,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
